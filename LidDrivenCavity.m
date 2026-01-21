8c9a0bb3b9b048491c4e44dc44b95cc93a696bff
Scorpio <duskscorpio@gmail.com>
2026-01-21 13:47:57 +0800

    Changed convection term to CDS

--- LidDrivenCavity.m
+++ LidDrivenCavity.m
@@ -196,7 +196,7 @@ while ~steadyReached && n < maxSteps
                     u_w_transient = 0;
                 end
     
-                convectionTerm = (u_w_interp * u_w - u_e_interp * u_e) / dx + (v_s_interp * u_s_interp - v_n_interp * u_n_interp) / dy; % LUDS
+                convectionTerm = (u_w_interp * u_w_interp - u_e_interp * u_e_interp) / dx + (v_s_interp * u_s_interp - v_n_interp * u_n_interp) / dy; % LUDS
                 diffusionTerm = (MU / RHO) * ((u_E - 2*u_P + u_W) / (dx * dx) + (u_N - 2*u_P + u_S) / (dy * dy));
                 pressureTerm = (p_w - p_e) / (RHO * dx);
     
@@ -270,7 +270,7 @@ while ~steadyReached && n < maxSteps
                     v_s_transient = 0;
                 end
     
-                convectionTerm = (u_w_interp * v_w_interp - u_e_interp * v_e_interp) / dx + (v_s_interp * v_s - v_n_interp * v_n) / dy; % LUDS
+                convectionTerm = (u_w_interp * v_w_interp - u_e_interp * v_e_interp) / dx + (v_s_interp * v_s_interp - v_n_interp * v_n_interp) / dy; % LUDS
                 diffusionTerm = (MU / RHO) * ((v_E - 2*v_P + v_W) / (dx * dx) + (v_N - 2*v_P + v_S) / (dy * dy));
                 pressureTerm = (p_s - p_n) / (RHO * dy);
                 v_star(i, j) = 0.25*(v_e_interp + v_w_interp + v_n_transient + v_s_transient) + dt * (convectionTerm + diffusionTerm + pressureTerm);
